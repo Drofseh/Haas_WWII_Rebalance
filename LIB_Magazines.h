f0e68ed58d482c7f802da3c9061b771080653548
Drofseh <ajhesford@hotmail.com>
2017-03-29 18:39:01 -0700

    Add files via upload

---
+++ LIB_Magazines.h
@@ -0,0 +1,244 @@
+class LIB_1Rnd_60mm_M6 : CA_LauncherMagazine {
+  descriptionShort = "2.36-Inch Anti-Tank Rocket M6, for use in Bazooka";
+  displayName = "Bazooka 2.36-Inch Anti-Tank Rocket M6";
+ };
+
+class LIB_1Rnd_RPzB : CA_LauncherMagazine {
+  descriptionShort = "88mm Gr. 4322 HEAT Rocket, for use in Panzerschreck RPzB 54";
+  displayName = "Panzerschreck 88mm Gr. 4322 HEAT Rocket";
+ };
+
+ class LIB_5Rnd_762x54 : CA_Magazine {
+  descriptionShort = "7.62x54mm, 5 Rounds, for use in Mosin Rifles";
+  displayName = "Mosin 5 Round Clip";
+ };
+  class LIB_5Rnd_762x54_b30 : LIB_5Rnd_762x54 {
+   descriptionShort = "7.62x54mm, 5 Rounds, B30 AP, for use in Mosin Rifles";
+   displayName = "Mosin 5 Round Clip, B30 AP";
+  };
+  class LIB_5Rnd_762x54_D : LIB_5Rnd_762x54 {
+   descriptionShort = "7.62x54mm, 5 Rounds, Type D Heavy, for use in Mosin Rifles";
+   displayName = "Mosin 5 Round Clip, Type D Heavy";
+  };
+  class LIB_5Rnd_762x54_t30 : LIB_5Rnd_762x54 {
+   descriptionShort = "7.62x54mm, 5 Rounds, T30 Tracer, for use in Mosin Rifles";
+   displayName = "Mosin 5 Round Clip, T30 Tracer";
+  };
+  class LIB_5Rnd_762x54_t46 : LIB_5Rnd_762x54 {
+   descriptionShort = "7.62x54mm, 5 Rounds, T46 Tracer, for use in Mosin Rifles";
+   displayName = "Mosin 5 Round Clip, T46 Tracer";
+  };
+
+ class LIB_5Rnd_762x63 : CA_Magazine {
+  descriptionShort = "7.62x63mm, 5 Rounds, for use in M1903 Springfield";
+  displayName = "M1903 Springfield 5 Round Clip";
+ };
+
+ class LIB_10Rnd_762x54 : LIB_5Rnd_762x54 {
+  descriptionShort = "7.62x54mm, 10 Rounds, for use in SVT-40";
+  displayName = "SVT-40 10 Round Magazine";
+ };
+  class LIB_10Rnd_762x54_b30 : LIB_10Rnd_762x54 {
+   descriptionShort = "7.62x54mm, 10 Rounds, AP, for use in SVT-40";
+   displayName = "SVT-40 10 Round Magazine, B30 AP";
+  };
+  class LIB_10Rnd_762x54_d : LIB_10Rnd_762x54 {
+   descriptionShort = "7.62x54mm, 10 Rounds, Type D Heavy, for use in SVT-40";
+   displayName = "SVT-40 10 Round Magazine, Type D Heavy";
+  };
+  class LIB_10Rnd_762x54_t30 : LIB_10Rnd_762x54 {
+   descriptionShort = "7.62x54mm, 10 Rounds, T30 Tracer, for use in SVT-40";
+   displayName = "SVT-40 10 Round Magazine, T30 Tracer";
+  };
+   class LIB_10Rnd_762x54_t302 : LIB_10Rnd_762x54_t30 {
+    descriptionShort = "7.62x54mm, 10 Rounds, Last Round T30 Tracer, for use in SVT-40";
+    displayName = "SVT-40 10 Round Magazine, Last Rnd T30 Tracer";
+   };
+  class LIB_10Rnd_762x54_t46 : LIB_10Rnd_762x54 {
+   descriptionShort = "7.62x54mm, 10 Rounds, T46 Tracer, for use in SVT-40";
+   displayName = "SVT-40 10 Round Magazine, T46 Tracer";
+  };
+   class LIB_10Rnd_762x54_t462 : LIB_10Rnd_762x54_t46 {
+    descriptionShort = "7.62x54mm, 10 Rounds, Last Round T46 Tracer, for use in SVT-40";
+    displayName = "SVT-40 10 Round Magazine, Last Rnd T46 Tracer";
+   };
+
+ class LIB_5Rnd_792x57 : CA_Magazine {
+  descriptionShort = "7.92x57mm, 5 Rounds, for use in K98k";
+  displayName = "K98k 5 Round Clip";
+ };
+  class LIB_5Rnd_792x57_SMK : LIB_5Rnd_792x57 {
+   descriptionShort = "7.92x57mm, 5 Rounds, S.m.K. AP, for use in K98k";
+   displayName = "K98k 5 Round Clip, S.m.K. AP";
+  };
+  class LIB_5Rnd_792x57_sS : LIB_5Rnd_792x57 {
+   descriptionShort = "7.92x57mm, 5 Rounds, S.m.E. Semi-AP, for use in K98k";
+   displayName = "K98k 5 Round Clip, S.m.E. Semi-AP";
+  };
+  class LIB_5Rnd_792x57_t : LIB_5Rnd_792x57 {
+   descriptionShort = "7.92x57mm, 5 Rounds, Tracer, for use in K98k";
+   displayName = "K98k 5 Round Clip, Tracer";
+  };
+
+ class LIB_10Rnd_792x57 : LIB_5Rnd_792x57 {
+  descriptionShort = "7.92x57mm, 10 Rounds, for use in G43";
+  displayName = "G43 10 Round Magazine";
+ };
+  class LIB_10Rnd_792x57_SMK : LIB_10Rnd_792x57 {
+   descriptionShort = "7.92x57mm, 10 Rounds, S.m.K. AP, for use in G43";
+   displayName = "G43 10 Round Magazine, S.m.K. AP";
+  };
+  class LIB_10Rnd_792x57_sS : LIB_10Rnd_792x57 {
+   descriptionShort = "7.92x57mm, 10 Rounds, S.m.E. Semi-AP, for use in G43";
+   displayName = "G43 10 Round Magazine, S.m.E. Semi-AP";
+  };
+  class LIB_10Rnd_792x57_T : LIB_10Rnd_792x57 {
+   descriptionShort = "7.92x57mm, 10 Rounds, Tracer, for use in G43";
+   displayName = "G43 10 Round Magazine, Tracer";
+  };
+  class LIB_10Rnd_792x57_T2 : LIB_10Rnd_792x57_T {
+   descriptionShort = "7.92x57mm, 10 Rounds, Last Round Tracer, for use in G43";
+   displayName = "G43 10 Round Magazine, Last Round Tracer";
+  };
+
+ class LIB_8Rnd_762x63 : CA_Magazine {
+  descriptionShort = ".30-06, 8 Rounds, for use in M1 Garand";
+  displayName = "M1 Garand, 8 Round Clip";
+ };
+
+ class LIB_15Rnd_762x33 : LIB_8Rnd_762x63 {
+  descriptionShort = ".30 Carbine, 15 Rounds, for use in M1 Carbine";
+  displayName = "M1 Carbine 15 Round Magazine";
+ };
+  class LIB_30Rnd_762x33_2PzD : LIB_15Rnd_762x33 {
+   count = 30;
+   descriptionShort = ".30 Carbine, 30 Rounds, for use in M2 Carbine";
+   displayName = "M2 Carbine 30 Round Magazine";
+  };
+
+ class LIB_20Rnd_762x63 : CA_Magazine {
+  descriptionShort = ".30-06, 20 Rounds, for use in M1918A2 BAR";
+  displayName = "M1918A2 BAR 20 Round Magazine";
+ };
+
+ class LIB_30Rnd_45ACP : CA_Magazine {
+  descriptionShort = ".45 ACP, 30 Rounds, for use in M1A1 Thompson";
+  displayName = "M1A1 Thompson 30 Round Magazine";
+ };
+  class LIB_20Rnd_45ACP_M1_2PzD : LIB_30Rnd_45ACP {
+   count = 20;
+   descriptionShort = ".45 ACP, 20 Rounds, for use in M1A1 Thompson";
+   displayName = "M1A1 Thompson 20 Round Magazine";
+  };
+
+ class LIB_30Rnd_792x33 : CA_Magazine {
+  descriptionShort = "7.95x33mm, 30 Rounds, for use in MP44";
+  displayName = "MP44 30 Round Magazine";
+ };
+
+ class LIB_32Rnd_9x19 : CA_Magazine {
+  descriptionShort = "9x19mm, 32 Rounds, for use in MP40";
+  displayName = "MP40 32 Round Magazine";
+ };
+
+ class LIB_35Rnd_762x25 : CA_Magazine {
+  descriptionShort = "7.62x25mm, 35 Rounds, for use in PPSh-41";
+  displayName = "PPSh-41 35 Round Magazine";
+ };
+  class LIB_35Rnd_762x25_ap : LIB_35Rnd_762x25 {
+   descriptionShort = "7.62x25mm, 35 Rounds, AP, for use in PPSh-41";
+   displayName = "PPSh-41 35 Round Magazine, AP";
+  };
+  class LIB_35Rnd_762x25_t : LIB_35Rnd_762x25 {
+   descriptionShort = "7.62x25mm, 35 Rounds, Tracer, for use in PPSh-41";
+   displayName = "PPSh-41 35 Round Magazine, Tracer";
+  };
+  class LIB_35Rnd_762x25_t2 : LIB_35Rnd_762x25_t {
+   descriptionShort = "7.62x25mm, 35 Rounds, Last Round Tracer, for use in PPSh-41";
+   displayName = "PPSh-41 35 Round Magazine, Last Round Tracer";
+  };
+
+ class LIB_47Rnd_762x54 : CA_Magazine {
+  descriptionShort = "7.62x54mm, 47 Rounds, for use in DP28";
+  displayName = "DP28 47 Round Magazine";
+ };
+  class LIB_47Rnd_762x54d : LIB_47Rnd_762x54 {
+   descriptionShort = "7.62x54mm, 47 Rounds, Type D Heavy, for use in DP28";
+   displayName = "DP28 47 Round Magazine, Type D Heavy";
+  };
+
+ class LIB_50Rnd_792x57 : CA_Magazine {
+  descriptionShort = "7.92x57mm, 50 Rounds, for use in MG42";
+  displayName = "MG42 50 Round Drum";
+ };
+  class LIB_50Rnd_792x57_SMK : LIB_50Rnd_792x57 {
+   descriptionShort = "7.92x57mm, 50 Rounds, S.m.K. AP, for use in MG42";
+   displayName = "MG42 50 Round Drum, S.m.K. AP";
+  };
+  class LIB_50Rnd_792x57_sS : LIB_50Rnd_792x57 {
+   descriptionShort = "7.92x57mm, 50 Rounds, S.m.E. Semi-AP, for use in MG42";
+   displayName = "MG42 50 Round Drum, S.m.E. Semi-AP";
+  };
+  class LIB_250Rnd_792x57 : LIB_50Rnd_792x57_Veh {
+   descriptionShort = "7.92x57mm, 250 Rounds, for use in MG42";
+   displayName = "MG42 250 Round Belt";
+  };
+  class LIB_250Rnd_792x57_SMK : LIB_250Rnd_792x57 {
+   descriptionShort = "7.92x57mm, 250 Rounds, S.m.K. AP, for use in MG42";
+   displayName = "MG42 250 Round Belt, S.m.K. AP";
+  };
+  class LIB_250Rnd_792x57_sS : LIB_250Rnd_792x57 {
+   descriptionShort = "7.92x57mm, 250 Rounds, s.S. Heavy, for use in MG42";
+   displayName = "MG42 250 Round Belt, s.S. Heavy";
+  };
+
+ class LIB_63Rnd_762x54 : LIB_47Rnd_762x54 {
+  count = 60;
+  descriptionShort = "7.62x54mm, 60 Rounds, for use in DT";
+  displayName = "DT 60 Round Magazine";
+ };
+  class LIB_63Rnd_762x54d : LIB_63Rnd_762x54 {
+   descriptionShort = "7.62x54mm, 60 Rounds, Type D Heavy, for use in DT";
+   displayName = "DT 60 Round Magazine, Type D Heavy";
+  };
+
+ class LIB_71Rnd_762x25 : CA_Magazine {
+  descriptionShort = "7.62x25mm, 71 Rounds, for use in PPSh-41";
+  displayName = "PPSh-41 71 Round Magazine";
+ };
+  class LIB_71Rnd_762x25_ap : LIB_71Rnd_762x25 {
+   descriptionShort = "7.62x25mm, 71 Rounds, AP, for use in PPSh-41";
+   displayName = "PPSh-41 71 Round Magazine, AP";
+  };
+  class LIB_71Rnd_762x25_t : LIB_71Rnd_762x25 {
+   descriptionShort = "7.62x25mm, 71 Rounds, Tracer, for use in PPSh-41";
+   displayName = "PPSh-41 71 Round Magazine, Tracer";
+  };
+  class LIB_71Rnd_762x25_t2 : LIB_71Rnd_762x25_t {
+   descriptionShort = "7.62x25mm, 71 Rounds, Last Round Tracer, for use in PPSh-41";
+   displayName = "PPSh-41 71 Round Magazine, Last Round Tracer";
+  };
+
+ class LIB_7Rnd_45ACP : CA_Magazine {
+  descriptionShort = ".45 ACP, 7 Rounds, for use in M1911A1";
+  displayName = "M1911A1 7 Round Magazine";
+ };
+
+ class LIB_7Rnd_762x38 : CA_Magazine {
+  descriptionShort = "7.62×38mm, 7 Rounds, for use in M1895 Nagant";
+  displayName = "M1895 Nagant 7 Rounds";
+ };
+
+ class LIB_8Rnd_762x25 : CA_Magazine {
+  descriptionShort = "7.62x25mm, 8 Rounds, for use in TT-33 Tokarev";
+  displayName = "TT-33 8 Round Magazine";
+ };
+
+ class LIB_8Rnd_9x19 : CA_Magazine {
+  descriptionShort = "9x19mm, 8 Rounds, for use in P38";
+  displayName = "P38 8 Round Magazine";
+ };
+  class LIB_8Rnd_9x19_P08 : LIB_8Rnd_9x19 {
+  descriptionShort = "9x19mm, 8 Rounds, for use in P08";
+  displayName = "P08 8 Round Magazine";
+  };
