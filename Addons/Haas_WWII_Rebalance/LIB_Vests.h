4bad47dbcce8fdd08b7aecb569e2c21d8c9c01ac
Drofseh <Drofseh@users.noreply.github.com>
2017-05-01 11:10:55 -0700

    Dev (#18)
    - Fix the inheritance problem in v1.5

--- Addons/Haas_WWII_Rebalance/LIB_Vests.h
+++ Addons/Haas_WWII_Rebalance/LIB_Vests.h
@@ -77,73 +77,73 @@
   };
 
  class V_LIB_DAK_FieldOfficer : V_LIB_GER_FieldOfficer {
-   class ItemInfo : VestItem {
+   class ItemInfo : ItemInfo {
    containerClass = "Supply70";
    };
   };
 
  class V_LIB_DAK_OfficerBelt : V_LIB_GER_OfficerBelt {
-   class ItemInfo : VestItem {
+   class ItemInfo : ItemInfo {
    containerClass = "Supply30";
    };
   };
 
  class V_LIB_DAK_OfficerVest : V_LIB_GER_OfficerVest {
-   class ItemInfo : VestItem {
+   class ItemInfo : ItemInfo {
    containerClass = "Supply70";
    };
   };
 
  class V_LIB_DAK_PioneerVest : V_LIB_GER_PioneerVest {
-   class ItemInfo : VestItem {
+   class ItemInfo : ItemInfo {
    containerClass = "Supply120";
    };
   };
 
  class V_LIB_DAK_PrivateBelt : V_LIB_GER_PrivateBelt {
-   class ItemInfo : VestItem {
+   class ItemInfo : ItemInfo {
    containerClass = "Supply30";
    };
   };
 
  class V_LIB_DAK_SniperBelt : V_LIB_GER_SniperBelt {
-   class ItemInfo : VestItem {
+   class ItemInfo : ItemInfo {
    containerClass = "Supply70";
    };
   };
 
  class V_LIB_DAK_VestG43 : V_LIB_GER_VestG43 {
-   class ItemInfo : VestItem {
+   class ItemInfo : ItemInfo {
    containerClass = "Supply90";
    };
   };
 
  class V_LIB_DAK_VestKar98 : V_LIB_GER_VestKar98 {
-   class ItemInfo : VestItem {
+   class ItemInfo : ItemInfo {
    containerClass = "Supply90";
    };
   };
 
  class V_LIB_DAK_VestMG : V_LIB_GER_VestMG {
-   class ItemInfo : VestItem {
+   class ItemInfo : ItemInfo {
    containerClass = "Supply120";
    };
   };
 
  class V_LIB_DAK_VestMP40 : V_LIB_GER_VestMP40 {
-   class ItemInfo : VestItem {
+   class ItemInfo : ItemInfo {
    containerClass = "Supply90";
    };
   };
 
  class V_LIB_DAK_VestSTG : V_LIB_GER_VestSTG {
-   class ItemInfo : VestItem {
+   class ItemInfo : ItemInfo {
    containerClass = "Supply90";
    };
   };
 
  class V_LIB_DAK_VestUnterofficer : V_LIB_GER_VestUnterofficer {
-   class ItemInfo : VestItem {
+   class ItemInfo : ItemInfo {
    containerClass = "Supply90";
    };
   };
@@ -151,6 +151,7 @@
  class V_LIB_SOV_IShBrVestMG : V_LIB_Vest_Camo_Base {
    class ItemInfo : VestItem {
    containerClass = "Supply60";
+   mass = 100;
     class HitpointsProtectionInfo {
      class Body {
       armor = 6;
@@ -162,8 +163,9 @@
   };
 
  class V_LIB_SOV_IShBrVestPPShDisc : V_LIB_SOV_IShBrVestMG {
-   class ItemInfo : VestItem {
+   class ItemInfo : ItemInfo {
    containerClass = "Supply90";
+   mass = 100;
     class HitpointsProtectionInfo {
      class Body {
       armor = 6;
@@ -175,8 +177,9 @@
   };
 
  class V_LIB_SOV_IShBrVestPPShMag : V_LIB_SOV_IShBrVestMG {
-   class ItemInfo : VestItem {
+   class ItemInfo : ItemInfo {
    containerClass = "Supply90";
+   mass = 100;
     class HitpointsProtectionInfo {
      class Body {
       armor = 6;
