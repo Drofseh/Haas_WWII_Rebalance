5590a2b079e04064057ed05ecb4313c93300de04
Drofseh <Drofseh@users.noreply.github.com>
2017-05-29 09:30:07 -0700

    Update most FOW and IFA backpack capacities. (#24)
    * Update version number to 1.6.0
    
    * Update most FOW and IFA backpack capacities.

--- Addons/Haas_WWII_Rebalance/FOW_Backpacks.h
+++ Addons/Haas_WWII_Rebalance/FOW_Backpacks.h
@@ -1,3 +1,119 @@
+ class fow_b_heer_aframe : Bag_Base {
+  maximumLoad = 100;
+  };
+
  class fow_b_heer_ammo_belt : Bag_Base {
   maximumLoad = 45;
+  };
+
+ class fow_b_ija_ammobox : Bag_Base {
+  maximumLoad = 500;
+  };
+
+ class fow_b_ija_ammobox_metal : Bag_Base {
+  maximumLoad = 500;
+  };
+
+ class fow_b_ija_ammobox_wood : Bag_Base {
+  maximumLoad = 500;
+  };
+
+ class fow_b_ija_backpack : Bag_Base {
+  maximumLoad = 150;
+  };
+
+ class fow_b_ija_backpack_medic : fow_b_ija_backpack {
+  maximumLoad = 150;
+  };
+
+ class fow_b_ija_backpack_rifleman : fow_b_ija_backpack {
+  maximumLoad = 150;
+  };
+
+ class fow_b_ija_backpack_asst_type99 : fow_b_ija_backpack {
+  maximumLoad = 150;
+  };
+
+ class fow_b_ija_backpack_foliage : fow_b_ija_backpack {
+  maximumLoad = 150;
+  };
+
+ class fow_b_ija_backpack_foliage_asst_type99 : fow_b_ija_backpack_foliage {
+  maximumLoad = 150;
+  };
+
+ class fow_b_ija_backpack_foliage_medic : fow_b_ija_backpack_foliage {
+  maximumLoad = 150;
+  };
+
+ class fow_b_ija_backpack_foliage_rifleman : fow_b_ija_backpack_foliage {
+  maximumLoad = 150;
+  };
+
+ class fow_b_uk_p37 : Bag_Base {
+  maximumLoad = 150;
+  };
+
+ class fow_b_uk_p37_blanco : fow_b_uk_p37 {
+  maximumLoad = 150;
+  };
+
+ class fow_b_uk_p37_blanco : fow_b_uk_p37 {
+  maximumLoad = 150;
+  };
+
+ class fow_b_uk_p37_medic : fow_b_uk_p37 {
+  maximumLoad = 150;
+  };
+
+ class fow_b_uk_p37_radio : fow_b_uk_p37 {
+  maximumLoad = 150;
+  };
+
+ class fow_b_uk_bergenpack : fow_b_uk_p37 {
+  maximumLoad = 250;
+  };
+
+ class fow_b_uk_bergenpack_medic : fow_b_uk_bergenpack {
+  maximumLoad = 250;
+  };
+
+ class fow_b_us_bandoleer : Bag_Base {
+  maximumLoad = 150;
+  };
+
+ class fow_b_us_m1928 : Bag_Base {
+  maximumLoad = 50;
+  };
+
+ class fow_b_us_m1928_asst_bar : fow_b_us_m1928 {
+  maximumLoad = 150;
+  };
+
+ class fow_b_us_m1928_medic : fow_b_us_m1928 {
+  maximumLoad = 150;
+  };
+
+ class fow_b_us_m1928_rifleman : fow_b_us_m1928 {
+  maximumLoad = 150;
+  };
+
+ class fow_b_us_rocket_bag : Bag_Base {
+  maximumLoad = 200;
+  };
+
+ class fow_b_us_m1928 : Bag_Base {
+  maximumLoad = 150;
+  };
+
+ class fow_b_usmc_m1928_asst_bar : fow_b_us_m1928 {
+  maximumLoad = 150;
+  };
+
+ class fow_b_usmc_m1928_medic : fow_b_us_m1928 {
+  maximumLoad = 150;
+  };
+
+ class fow_b_usmc_m1928_rifleman : fow_b_us_m1928 {
+  maximumLoad = 150;
   };
--- Addons/Haas_WWII_Rebalance/LIB_Backpacks.h
+++ Addons/Haas_WWII_Rebalance/LIB_Backpacks.h
@@ -1,15 +1,168 @@
+
+  class B_LIB_GER_A_frame : B_LIB_AssaultPack_Base {
+  maximumLoad = 100;
+  };
+  
+  class B_LIB_GER_A_frame_AT : B_LIB_GER_A_frame {
+  maximumLoad = 100;
+  };
+  
+  class B_LIB_GER_A_frame_Gef : B_LIB_GER_A_frame {
+  maximumLoad = 100;
+  };
+  
+  class B_LIB_GER_A_frame_kit : B_LIB_GER_A_frame {
+  maximumLoad = 100;
+  };
+  
+  class B_LIB_GER_A_frame_zeltbahn : B_LIB_GER_A_frame {
+  maximumLoad = 100;
+  };
+
+  class B_LIB_DAK_A_frame : B_LIB_GER_A_frame {
+  maximumLoad = 100;
+  };
+
+ class B_LIB_DAK_A_frame_AT : B_LIB_DAK_A_frame {
+  maximumLoad = 100;
+  };
+
+ class B_LIB_DAK_A_frame_Gef : B_LIB_DAK_A_frame {
+  maximumLoad = 100;
+  };
+
+ class B_LIB_DAK_A_frame_kit : B_LIB_GER_A_frame_kit {
+  maximumLoad = 100;
+  };
+
+ class B_LIB_GER_Backpack : B_LIB_AssaultPack_Base {
+  maximumLoad = 200;
+  };
+
  class B_LIB_GER_MedicBackpack : B_LIB_AssaultPack_Base {
-  maximumLoad = 250;
+  maximumLoad = 200;
   };
 
  class B_LIB_GER_MedicBackpack_Empty : B_LIB_GER_MedicBackpack {
-  maximumLoad = 250;
+  maximumLoad = 200;
   };
 
  class B_LIB_GER_MedicBackpack_Big_Empty : B_LIB_GER_MedicBackpack_Empty {
-  maximumLoad = 250;
+  maximumLoad = 200;
+  };
+
+ class B_LIB_GER_Panzer : B_LIB_GER_MedicBackpack_Empty {
+  maximumLoad = 375;
+  };
+
+ class B_LIB_GER_Panzer_Empty : B_LIB_GER_Panzer {
+  maximumLoad = 375;
+  };
+
+ class B_LIB_GER_Panzer_Big_Empty : B_LIB_GER_Panzer_Empty {
+  maximumLoad = 375;
   };
 
  class B_LIB_GER_Tonister34_cowhide : B_LIB_GER_MedicBackpack_Empty {
-  maximumLoad = 250;
+  maximumLoad = 200;
+  };
+
+ class B_LIB_GER_SapperBackpack_empty : B_LIB_GER_Backpack {
+  maximumLoad = 200;
+  };
+
+ class B_LIB_GER_SapperBackpack : B_LIB_GER_SapperBackpack_empty {
+  maximumLoad = 200;
+  };
+
+ class B_LIB_GER_SapperBackpack2 : B_LIB_GER_SapperBackpack_empty {
+  maximumLoad = 200;
+  };
+
+ class B_LIB_SOV_RA_GasBag : B_LIB_AssaultPack_Base {
+  maximumLoad = 150;
+  };
+
+ class B_LIB_SOV_RA_MedicalBag : B_LIB_AssaultPack_Base {
+  maximumLoad = 100;
+  };
+
+ class B_LIB_SOV_RA_MedicalBag_Empty : B_LIB_SOV_RA_MedicalBag {
+  maximumLoad = 100;
+  };
+
+ class B_LIB_SOV_RA_MedicalBag_Big_Empty : B_LIB_SOV_RA_MedicalBag_Empty {
+  maximumLoad = 100;
+  };
+
+ class B_LIB_SOV_RA_MGAmmoBag : B_LIB_AssaultPack_Base {
+  maximumLoad = 150;
+  };
+
+ class B_LIB_SOV_RA_MGAmmoBag_Empty : B_LIB_SOV_RA_MGAmmoBag {
+  maximumLoad = 150;
+  };
+
+ class B_LIB_SOV_RA_MGAmmoBag_Big_Empty : B_LIB_SOV_RA_MGAmmoBag_Empty {
+  maximumLoad = 150;
+  };
+
+ class B_LIB_SOV_RA_Rucksack : B_LIB_AssaultPack_Base {
+  maximumLoad = 150;
+  };
+
+ class B_LIB_SOV_RA_Rucksack_Green : B_LIB_SOV_RA_Rucksack {
+  maximumLoad = 150;
+  };
+
+ class B_LIB_SOV_RA_Rucksack2 : B_LIB_SOV_RA_Rucksack {
+  maximumLoad = 150;
+  };
+
+ class B_LIB_SOV_RA_Rucksack2_Green : B_LIB_SOV_RA_Rucksack2 {
+  maximumLoad = 150;
+  };
+
+ class B_LIB_SOV_RA_Shinel : B_LIB_SOV_RA_Rucksack {
+  maximumLoad = 100;
+  };
+
+ class B_LIB_US_Backpack : B_LIB_AssaultPack_Base {
+  maximumLoad = 150;
+  };
+
+ class B_LIB_US_Backpack_dday : B_LIB_US_Backpack {
+  maximumLoad = 150;
+  };
+
+ class B_LIB_US_Backpack_eng : B_LIB_US_Backpack {
+  maximumLoad = 150;
+  };
+
+ class B_LIB_US_Bandoleer : B_LIB_AssaultPack_Base {
+  maximumLoad = 50;
+  };
+
+ class B_LIB_US_MGbag : B_LIB_AssaultPack_Base {
+  maximumLoad = 150;
+  };
+
+ class B_LIB_US_MGbag_Empty : B_LIB_US_MGbag {
+  maximumLoad = 150;
+  };
+
+ class B_LIB_US_MGbag_Big_Empty : B_LIB_US_MGbag_Empty {
+  maximumLoad = 150;
+  };
+
+ class B_LIB_US_RocketBag : B_LIB_AssaultPack_Base {
+  maximumLoad = 200;
+  };
+
+ class B_LIB_US_RocketBag_Empty : B_LIB_US_RocketBag {
+  maximumLoad = 200;
+  };
+
+ class B_LIB_US_RocketBag_Big_Empty : B_LIB_US_RocketBag_Empty {
+  maximumLoad = 200;
   };
