2cc0dfe1baf4a90e392d5485e5382818c5cacc01
Drofseh <Drofseh@users.noreply.github.com>
2020-04-23 15:34:19 -0700

    initial framework for optional sound addons

---
+++ Optionals/Haas_JSRS_Audio_Expansion/config.cpp
@@ -0,0 +1,30 @@
+﻿
+class CfgPatches {
+    class Haas_JSRS_Audio_Expansion {
+        author = "Wilhelm Haas (Drofseh)";
+        authors[] = {"Wilhelm Haas (Drofseh)"};
+        units[] = {};
+        magazineWell[] = {};
+        magazines[] = {};
+        weapons[] = {};
+        requiredAddons[] = {"Haas_WWII_Rebalance"}; // add JSRS IFA dependancy
+    };
+}; // End CfgPatches
+
+class BaseSoundModeType;
+
+class Eventhandlers;
+
+class Mode_SemiAuto;
+
+class Mode_Burst;
+
+class Mode_FullAuto;
+
+class SlotInfo;
+
+class cfgWeapons {
+
+    #include "cfgWeapons.h"
+
+};
