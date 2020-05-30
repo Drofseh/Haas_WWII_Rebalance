5fbf9ff34f5f21efc529a2d4576a2f426a738786
Drofseh <Drofseh@users.noreply.github.com>
2020-05-30 10:42:03 -0700

    update version number to 1.29.1 (#81)

--- mod.cpp
+++ mod.cpp
@@ -1,6 +1,6 @@
 name = "Haas WWII Rebalance";
 author = "Wilhelm Haas (2PzD)";
-description = "Haas WWII Rebalance - Version 1.29.0";
+description = "Haas WWII Rebalance - Version 1.29.1";
 overview = "Haas WWII Rebalance correct, enhances, or adds compatibility for IFA3, FOW, LEN, CSA38, and Sep39.";
 logo = "Images\logo.paa";
 logoOver = "Images\logo.paa";
